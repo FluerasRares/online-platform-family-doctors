47746fcaa7b909aa62931e4ad15dea5039d48265
ovi@sharetini-at-580853569485 <ovi.comes@gmail.com>
2020-05-09 17:45:25 -0400

    directory tweaks

--- contacts/src/components/Medics/api.js
+++ contacts/src/components/Medics/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { medic } = require('../../apicfg.json');
 
 export const fetchAll = async () => {
-  // const { data: response } = await axios.request({ baseURL: medic.baseURL, ...medic.fetchAll });
-  // return response;
-  return [{ id: 1, name: 'Ion Ionescu', specialitate: 'chirurg' }, { id: 2, name: 'Mihai Vasilescu', specialitate: 'ortoped' }];
+  const { data: response } = await axios.request({ baseURL: medic.baseURL, ...medic.fetchAll });
+  return response.slice(0, 10);
+  // return [{ id: 1, name: 'Ion Ionescu', specialitate: 'chirurg' }, { id: 2, name: 'Mihai Vasilescu', specialitate: 'ortoped' }];
 }
--- contacts/src/components/Medics/index.js
+++ contacts/src/components/Medics/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Card, CardHeader, CardContent, List, ListItem, ListItemText, ListItemAvatar, Avatar, Grid } from '@material-ui/core';
+import { Card, CardHeader, CardContent, List, ListItem, ListItemText, ListItemAvatar, Avatar, Grid, ListItemSecondaryAction, IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -8,6 +8,7 @@ import styles from './styles';
 import LeftMenu from '../LeftMenu';
 import Chat from '../Chat';
 import { selectAllMedics } from './selectors';
+import ChatIcon from '@material-ui/icons/Chat';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 
 const Medics = ({ medics = [] }) => {
@@ -18,7 +19,8 @@ const Medics = ({ medics = [] }) => {
         <Card>
           <CardHeader title="Medici" style={{ background: '#f1f1f1' }} />
           <CardContent>
-            <BootstrapTable data={medics}
+
+            {/* <BootstrapTable data={medics}
               border={false}
               height='120'
               scrollTop={'Bottom'}
@@ -26,16 +28,21 @@ const Medics = ({ medics = [] }) => {
               <TableHeaderColumn dataField='id'
                 isKey={true}>ID</TableHeaderColumn>
               <TableHeaderColumn dataField='name'>Nume / Prenume</TableHeaderColumn>
-            </BootstrapTable>
+            </BootstrapTable> */}
 
             <List component="nav">
               {medics
-                .map(({ id, name, specialitate }, index) =>
+                .map(({ id, name, departament: specialitate }, index) =>
                   <ListItem button key={index}>
                     <ListItemAvatar>
                       <Avatar alt={name} src="/static/images/avatar/1.jpg" />
                     </ListItemAvatar>
                     <ListItemText primary={name} secondary={specialitate} style={{ display: 'inline'}}/>
+                    <ListItemSecondaryAction>
+                      <IconButton edge="end" aria-label="chat">
+                        <ChatIcon/>
+                      </IconButton>
+                    </ListItemSecondaryAction>                    
                   </ListItem>
               )}
             </List>
