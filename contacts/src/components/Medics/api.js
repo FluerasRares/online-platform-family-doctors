787979899919b4c6a9e6f13d910139531c1a06ce
ovi@sharetini-at-580853569485 <ovi.comes@gmail.com>
2020-05-09 20:08:36 -0400

    fix video chat user

--- contacts/src/components/Medics/api.js
+++ contacts/src/components/Medics/api.js
@@ -3,12 +3,7 @@ const { medic } = require('../../apicfg.json');
 
 export const fetchAll = async () => {
   const { data: response } = await axios.request({ baseURL: medic.baseURL, ...medic.fetchAll });
-<<<<<<< HEAD
-  // return response.slice(0, 10);
-  return [{ id: 1, name: 'Ion Ionescu', specialitate: 'chirurg' }, { id: 2, name: 'Mihai Vasilescu', specialitate: 'ortoped' }];
-=======
   return response;
   // return response.slice(0, 10);
   // return [{ id: 1, name: 'Ion Ionescu', specialitate: 'chirurg' }, { id: 2, name: 'Mihai Vasilescu', specialitate: 'ortoped' }];
->>>>>>> cb985484ebd17c9ba862c460607d827bcb469a95
 }
--- contacts/src/components/Medics/index.js
+++ contacts/src/components/Medics/index.js
@@ -1,39 +1,35 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
-import { Card, CardHeader, CardContent, List, ListItem, ListItemText, ListItemAvatar, Avatar, Grid, ListItemSecondaryAction, IconButton, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import { Card, CardContent, CardHeader, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-=======
-import React, {Fragment} from 'react';
-import {Card, CardContent, CardHeader, Grid} from '@material-ui/core';
-import {withStyles} from '@material-ui/core/styles';
-import {Link, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {compose} from 'redux';
->>>>>>> cb985484ebd17c9ba862c460607d827bcb469a95
 import styles from './styles';
 import LeftMenu from '../LeftMenu';
 import Chat from '../Chat';
-import {selectAllMedics} from './selectors';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+import { selectAllMedics } from './selectors';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { selectCurrentUser } from '../User/selectors';
 
-<<<<<<< HEAD
-const Medics = ({ medics = [] }) => {
-  const [open, setOpen] = React.useState(false);
-  const [chatWith, setChatWith] = React.useState('');
+// user = row
+function actionFormatter(cell, user, {history, currentUser}) {
 
-  const handleClickOpen = name => {
-    console.log(name)
-    setChatWith(name);
-    setOpen(true);
-  };
+  // we should check if user is active or not (for the moment we presume all are active)
+  return (
+    <div>
+      <React.Fragment>
+        <Link to={"#"}
+          onClick={e => history.push(`/test-video/${currentUser.prenume} ${currentUser.nume}`)}
+        >
+          Video
+                </Link>
+      </React.Fragment>
+    </div>
+  )
+}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  
+const Medics = ({ medics = [], history, currentUser = {} }) => {
+
   return <Fragment>
     <Grid container spacing={1}>
       <Grid item xs={2}><LeftMenu /></Grid>
@@ -41,130 +37,42 @@ const Medics = ({ medics = [] }) => {
         <Card>
           <CardHeader title="Medici" style={{ background: '#f1f1f1' }} />
           <CardContent>
-
-            {/* <BootstrapTable data={medics}
+            <BootstrapTable data={medics}
               border={false}
-              height='120'
+              // height='120'
               scrollTop={'Bottom'}
               pagination
               search>
               <TableHeaderColumn dataField='id'
                 isKey={true}>ID</TableHeaderColumn>
+              <TableHeaderColumn dataField='cmi'>CMI</TableHeaderColumn>
               <TableHeaderColumn dataField='name'>Nume / Prenume</TableHeaderColumn>
-            </BootstrapTable> */}
+              <TableHeaderColumn dataField='address'>Adresa</TableHeaderColumn>
+              <TableHeaderColumn dataField='phone'>Telefon</TableHeaderColumn>
+              <TableHeaderColumn dataField='email'>Email</TableHeaderColumn>
+              <TableHeaderColumn dataField='department'>Departament</TableHeaderColumn>
+              <TableHeaderColumn dataField='actions'
+                dataFormat={actionFormatter}
+                formatExtraData={{ history, currentUser }} >Actiuni</TableHeaderColumn>
+            </BootstrapTable>
 
-            <List component="nav">
-=======
-// user = row
-function actionFormatter(cell, user, history) {
-
-    // we should check if user is active or not (for the moment we presume all are active)
-    return (
-        <div>
-            <React.Fragment>
-                <Link to={"#"}
-                      onClick={e => history.push('/test-video/' + user.name)}
-                >
-                    Video
-                </Link>
-            </React.Fragment>
-        </div>
-    )
-}
-
-const Medics = ({medics = [], history}) => {
-
-    return <Fragment>
-        <Grid container spacing={1}>
-            <Grid item xs={2}><LeftMenu/></Grid>
-            <Grid item xs={10}>
-                <Card>
-                    <CardHeader title="Medici" style={{background: '#f1f1f1'}}/>
-                    <CardContent>
-
-                        <BootstrapTable data={medics}
-                                        border={false}
-                            // height='120'
-                                        scrollTop={'Bottom'}
-                                        pagination
-                                        search>
-                            <TableHeaderColumn dataField='id'
-                                               isKey={true}>ID</TableHeaderColumn>
-                            <TableHeaderColumn dataField='cmi'>CMI</TableHeaderColumn>
-                            <TableHeaderColumn dataField='name'>Nume / Prenume</TableHeaderColumn>
-                            <TableHeaderColumn dataField='address'>Adresa</TableHeaderColumn>
-                            <TableHeaderColumn dataField='phone'>Telefon</TableHeaderColumn>
-                            <TableHeaderColumn dataField='email'>Email</TableHeaderColumn>
-                            <TableHeaderColumn dataField='department'>Departament</TableHeaderColumn>
-                            <TableHeaderColumn dataField='actions'
-                                               dataFormat={actionFormatter}
-                                               formatExtraData={history}>Actiuni</TableHeaderColumn>
-                        </BootstrapTable>
-
-                        {/* <List component="nav">
->>>>>>> cb985484ebd17c9ba862c460607d827bcb469a95
-              {medics
-                .map(({ id, name, departament: specialitate }, index) =>
-                  <ListItem button key={index}>
-                    <ListItemAvatar>
-                      <Avatar alt={name} src="../../assests/1.jpg" />
-                    </ListItemAvatar>
-                    <ListItemText primary={name} secondary={specialitate} style={{ display: 'inline'}}/>
-                    <ListItemSecondaryAction>
-                      <IconButton edge="end" aria-label="chat">
-                        <ChatIcon onClick={e => handleClickOpen(name)}/>
-                      </IconButton>
-                    </ListItemSecondaryAction>                    
-                  </ListItem>
-              )}
-<<<<<<< HEAD
-            </List>
           </CardContent>
         </Card>
       </Grid>
     </Grid>
-
-    <div>
-      {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open chat
-      </Button> */}
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{`Start chat with ${chatWith}`}</DialogTitle>
-        <DialogContent>
-          <div id="chatlioWidgetPlaceholder" style={{ width: 450, height: 300 }}></div>      
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>      
+    <Chat />
   </Fragment>
-=======
-            </List> */}
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-        <Chat/>
-    </Fragment>
->>>>>>> cb985484ebd17c9ba862c460607d827bcb469a95
 }
 
 const mapStateToProps = state => ({
-    medics: selectAllMedics(state),
+  medics: selectAllMedics(state),
+  currentUser: selectCurrentUser(state),
 });
 
 const mapDispatchToProps = {};
 
 export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    withStyles(styles, {withTheme: true}),
-    withRouter,
+  connect(mapStateToProps, mapDispatchToProps),
+  withStyles(styles, { withTheme: true }),
+  withRouter,
 )(Medics);
